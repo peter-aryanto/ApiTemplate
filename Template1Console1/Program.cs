9ef9f2cdb01c40693ed1132979f571d53b66a915
Peter Aryanto <peter.aryanto@gmail.com>
2024-04-20 14:47:36 +1000

    dotnet new console dotnet add reference ..\Template1\Template1.csproj

---
+++ Template1Console1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Template1.Logics;
+
+void Show(object o = null)
+{
+    if (o != null && !o.GetType().IsPrimitive)
+    {
+        o = System.Text.Json.JsonSerializer.Serialize(o);
+    }
+    Console.WriteLine($"|{o?.ToString()}|{Environment.NewLine}");
+}
+
+var forecast = new WeatherForecastLogic().Get();
+Show(forecast);

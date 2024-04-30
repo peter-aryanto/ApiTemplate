c0a2176448aacb2826c01bb3035d5abaa2cbef31
Peter Aryanto <peter.aryanto@gmail.com>
2024-04-30 10:51:05 +1000

    add process

---
+++ Template1Console1/Logic/Process01.cs
@@ -0,0 +1,13 @@
+using Template1Console1.Utils;
+
+namespace Template1Console1;
+
+public interface IProcess01
+{
+    //
+}
+
+public class Process01 : IProcess01
+{
+    //
+}
---
+++ Template1UnitTests/Console1/Logic/Process01Tests.cs
@@ -0,0 +1,19 @@
+using Template1Console1;
+
+namespace Template1UnitTests.Console1;
+
+public class Process01Tests
+{
+    private IProcess01 sut;
+
+    public Process01Tests()
+    {
+        sut = new Process01();
+    }
+
+    [Fact]
+    public void TestSomething()
+    {
+        // Assert.True(sut.DoProcess());
+    }
+}
